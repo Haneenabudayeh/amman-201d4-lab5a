95a8121b445c659ee9ad66296a145961dcbe3984
haneenabudayeh <haneenabudayeh5@gmail.com>
2020-02-20 14:35:12 +0200

    add sumarray

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    var theSum = a+b;
+    var theSum = a + b;
     // 'The sum of 4 and 7 is 11.'
     var message = `The sum of ${a} and ${b} is ${theSum}.`;
     return [theSum, message];
@@ -33,8 +33,8 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) {
     var theproduct = a * b;
 
-var mass = `The product of ${a} and ${b} is ${theproduct}.`;
-return [theproduct, mass];
+    var mass = `The product of ${a} and ${b} is ${theproduct}.`;
+    return [theproduct, mass];
 }
 testMultiply();
 
@@ -56,19 +56,19 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { 
-    var summation1=sum(a,b)[0];
-    var summation2=sum(summation1,c)[0];
-   
-    var theproduct1=multiply(a,b)[0];
-    var theproduct2=multiply(theproduct1,c)[0];
-    
-    var thirdElemant="4 and 7 and 5 sum to 16.";
-    var fourthElemant="The product of 4 and 7 and 5 is 140.";
+function sumAndMultiply(a, b, c) {
+    var summation1 = sum(a, b)[0];
+    var summation2 = sum(summation1, c)[0];
 
-    return[summation2,theproduct2,thirdElemant,fourthElemant];
+    var theproduct1 = multiply(a, b)[0];
+    var theproduct2 = multiply(theproduct1, c)[0];
+
+    var thirdElemant = "4 and 7 and 5 sum to 16.";
+    var fourthElemant = "The product of 4 and 7 and 5 is 140.";
+
+    return [summation2, theproduct2, thirdElemant, fourthElemant];
 }
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
@@ -86,11 +86,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
-
-function sumArray(sumArr) { //eslint-disable-line
-
+var testArray = [2, 3, 4];
+function sumArray(sumArr) {
+    var solve1 = sum(testArray[0], testArray[1])[0];
+    var solve2 = sum(solve1, testArray[2])[0];
+    var massagee = '2,3,4 was passed in as an array of numbers, and 9 is their sum.';
+    return [ solve2, massagee ];
 }
+testSumArray();
 
 // Here is the test for sumArray(); uncomment it to run it
 
@@ -135,7 +138,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
